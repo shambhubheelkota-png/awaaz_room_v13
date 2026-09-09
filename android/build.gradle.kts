5d4dfeeb491ea370fdca60baba5cdcab3fdc6054
shambhubheelkota-png <shambhubheelkota@gmail.com>
2026-09-09 22:39:43 +0530

    Refactor build.gradle.kts for AGP 9.0 compatibility
    Refactor build.gradle.kts to be AGP 9.0 compatible and update project structure.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,37 +1,18 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.awaaz_room"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "com.example.awaaz_room"
-        minSdk = 23
-        targetSdk = 36
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-flutter {
-    source = "../../.."
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// AGP 9.0 compatible clean task
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
